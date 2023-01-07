a2ea3661031d6d7e7c97603c8e23ab370134cfee
Sameel <sameelnaqvi@gmail.com>
2023-01-07 09:36:24 -0600

    Set up Firebase Upgraded to Compose 1.3.0

--- app/src/main/java/com/example/tastebud/MainActivity.kt
+++ app/src/main/java/com/example/tastebud/MainActivity.kt
@@ -19,7 +19,7 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            TasteBudTheme() {
+            TasteBudTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
